1622814c6d2d58defeefb2ae7b04a94c281c82ae
healthycounciladvisor <jimm.dev2323@gmail.com>
2023-11-17 11:32:48 +0100

    react-todos-app

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,12 @@
 import { memo } from "react";
+import styles from './my-style.module.css';
 
 const Todos = ({ todos }) => {
   console.log("child render");
 
   return (
     <>
-      <h2>My Todos</h2>
+      <h2 className={styles.bigblue}>My Todos</h2>
       {todos.map((todo, index) => {
         return <p key={index}>{todo}</p>;
       })}
